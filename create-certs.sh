0878fd76c752151437faed8a014256e476b4558a
Irving Santiago <irving.santiago@paidy.com>
2020-02-16 14:25:10 +0900

    add localhost to the keystore/trustore

--- create-certs.sh
+++ create-certs.sh
@@ -9,6 +9,7 @@ dname_tail="OU=Dev, O=TEST, L=Tokyo, S=Tokyo, C=JP"
 crt=cert.crt
 crtkey="temp.$crt.key"
 
+rm -rf temp*
 rm -f cert*
 # Generate CA key
 openssl req -new -x509 -keyout $crtkey -out $crt -days 9999 -subj $dname -passin pass:$pass -passout pass:$pass
@@ -42,7 +43,31 @@ do
 	# Create truststore and import the CA crt.
 	keytool -keystore ./$i/truststore.jks -alias CARoot -import -file $crt -storepass $pass -keypass $pass -noprompt
 
+  if [ "$i" = "kafka" ]; then
+	  keytool -genkey -noprompt \
+	  			 -alias localhost \
+	  			 -dname "CN=localhost, $dname_tail" \
+	  			 -keystore ./$i/localhost.keystore.jks \
+	  			 -keyalg RSA \
+	  			 -storepass $pass \
+	  			 -keypass $pass
+    csr="temp.localhost.csr"
+    signedcrt="temp.localhost.crt"
+	  # Create CSR, sign the key and import back into keystore
+	  keytool -keystore ./$i/localhost.keystore.jks -alias localhost -certreq -file $csr -storepass $pass -keypass $pass -noprompt
+    openssl x509 -req -CA $crt -CAkey $crtkey -in $csr -out $signedcrt -days 9999 -CAcreateserial -passin pass:$pass
+	  keytool -keystore ./$i/localhost.keystore.jks -alias CARoot_localhost -import -file $crt -storepass $pass -keypass $pass -noprompt
+    keytool -keystore ./$i/localhost.keystore.jks -alias localhost -import -file $signedcrt  -storepass $pass -keypass $pass -noprompt
+
+	  # Create truststore and import the CA crt.
+	  keytool -keystore ./$i/localhost.truststore.jks -alias CARoot_localhost -import -file $crt -storepass $pass -keypass $pass -noprompt
+
+    keytool -importkeystore -srckeystore ./$i/localhost.keystore.jks -destkeystore ./$i/keystore.jks -srcstorepass $pass -deststorepass $pass -noprompt
+    keytool -exportcert -keystore ./$i/localhost.truststore.jks -alias CARoot_localhost -file temp.localhost.temp.cert -storepass $pass -noprompt
+    keytool -importcert -keystore ./$i/truststore.jks -alias CARoot_localhost -file temp.localhost.temp.cert -storepass $pass -noprompt
+  fi
+
   echo "$pass" > ./$i/creds
 done
 
-rm -rf temp*
+#rm -rf temp*
